e636c4ad8d405ec26880bc383223e99e06f87862
robbertvdzon <robbert.van.der.zon@ah.nl>
2023-05-26 07:27:24 +0200

    initial commit

---
+++ scripts/test_display.py
@@ -0,0 +1,9 @@
+import I2C_LCD_driver
+from time import *
+
+mylcd = I2C_LCD_driver.lcd()
+
+mylcd.lcd_display_string("Hello World!", 1)
+mylcd.lcd_display_string("Hello World!2", 2)
+mylcd.lcd_display_string("Hello World!3", 3)
+mylcd.lcd_display_string("Hello World!4", 4)
