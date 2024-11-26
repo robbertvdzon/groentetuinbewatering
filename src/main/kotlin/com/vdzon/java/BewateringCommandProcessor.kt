bcad526766026a12f2a8186471dda118479c6c6f
Robbert vd Zon <robbert.vanderzon@edsn.nl>
2024-11-26 20:34:19 +0100

    update

--- src/main/kotlin/com/vdzon/java/BewateringCommandProcessor.kt
+++ src/main/kotlin/com/vdzon/java/BewateringCommandProcessor.kt
@@ -6,6 +6,16 @@ import com.vdzon.java.common.CommandProcessor
 class BewateringCommandProcessor(val controller: Controller) : CommandProcessor {
     override fun process(command: String) {
         println("Processing command: $command")
+
+        val count = command.toIntOrNull()
+        if (count!=null && count>0){
+            println("open $count minuten meer")
+            controller.encoderUp(count)
+        }
+        if (count!=null && count<0){
+            println("open $count minuten minder")
+            controller.encoderDown(count*-1)
+        }
         if (command == "open") {
             controller.openKlep()
         } else if (command == "close") {
